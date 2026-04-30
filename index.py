e3fd03ad7f7f929b154d0db31ffeb9697c5d9d6e
Vamsi Chintha <74341557+Vamsi3prog@users.noreply.github.com>
2026-04-30 14:17:09 +0530

    Update index.py
    added a for loop and count of critical incidents

--- index.py
+++ index.py
@@ -1,10 +1,17 @@
 import random
 
-temp = random.uniform(100)
+count = 0
 
-if temp <= 32 :
-  print("Normal")
-elif temp <= 60 :
-  print("Moderate")
-else :
-  print("Critical")
+for i in range(10) : 
+    
+  temp = random.uniform(100)
+  
+  if temp <= 32 :
+    print("Normal")
+  elif temp <= 60 :
+    print("Moderate")
+  else :
+    print("Critical")
+    count += 1
+
+print("No.of critical incidents : ", count)
