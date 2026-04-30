23a2be11e9fca83669480d126960ebd34a49c194
Vamsi Chintha <74341557+Vamsi3prog@users.noreply.github.com>
2026-04-30 15:31:51 +0530

    Update index.py
    Added class

--- index.py
+++ index.py
@@ -3,21 +3,36 @@ import time
 
 start = time.ctime()
 count = 0
+n = random.randint(5, 11)
 
-for i in range(5) : 
+class TemperatureSensor :
+  def __init__(self, id):
+    self.sensor_id = id
+
+  def temperature(self) :
+    temp = 0
+    count = 0
+
+    for i in range(n) : 
     
-  temp = random.randint(0, 100)
+      temp = random.randint(0, 100)
   
-  if temp <= 32 :
-    print(f"{time.ctime()} : {temp} C -- Normal")
-  elif temp <= 60 :
-    print(f"{time.ctime()} : {temp} C -- Moderate")
-  else :
-    print(f"{time.ctime()} : {temp} C -- Critical")
-    with open("log.txt", "a") as log_file :
+    if temp <= 32 :
+      print(f"{time.ctime()} : {temp} C -- Normal")
+    elif temp <= 60 :
+      print(f"{time.ctime()} : {temp} C -- Moderate")
+    else :
+      print(f"{time.ctime()} : {temp} C -- Critical")
+      with open("log.txt", "a") as log_file :
         log_file.write(f"{time.strftime('%Y-%m-%d %H:%M:%S')} - Critical Temperature: {temp}\n")
-    count += 1
+      count += 1 # not being counted 
 
-  time.sleep(2)
+    time.sleep(2)
 
-print(f"No.of critical incidents from {start} to {time.ctime()} : {count}")
+  print(f"No.of critical incidents from {start} to {time.ctime()} : {count}")
+
+
+system1 = TemperatureSensor(1)
+print("System ID : ", system1.sensor_id)
+for i in range(n) : 
+  system1.temperature()
