b93d03730c2db5145ea1f092ec5acb3e535dcbd8
Vamsi Chintha <74341557+Vamsi3prog@users.noreply.github.com>
2026-05-16 15:25:19 +0530

    Update index.py
    Improved indentation

--- index.py
+++ index.py
@@ -3,45 +3,49 @@ import time
 
 start = time.ctime()
 
-class TemperatureSensor :
-    
-  NORMAL = 32
-  MODERATE = 60
-    
-  def __init__(self, id):
-      self.sensor_id = id
-      self.count = 0
 
-  def get_temperature(self) :
-      
-    self.temp = random.randint(0, 100)
+class TemperatureSensor:
 
-  def log_writer(self) :
-      with open("log.txt", "a") as log_file :
-        log_file.write(f"{time.strftime('%Y-%m-%d %H:%M:%S')} - Critical Temperature: {self.temp}\n")
-      self.count += 1
+    NORMAL = 32
+    MODERATE = 60
 
-  def classifier(self) :
-    
-    if self.temp <= self.NORMAL : 
-        print(f"{time.ctime()} : {self.temp} C -- Normal")
-    elif (self.temp <= self.MODERATE) : 
-        print(f"{time.ctime()} : {self.temp} C -- Moderate")
-    else :
-        print(f"{time.ctime()} : {self.temp} C -- Critical")
-        self.log_writer()
+    def __init__(self, id):
+        self.sensor_id = id
+        self.count = 0
 
-  def monitor(self) :
-    
-    self.get_temperature()
-    
-    self.classifier()
-    
-    time.sleep(2)
+    def get_temperature(self):
+
+        self.temp = random.randint(0, 100)
+
+    def log_writer(self):
+        with open("log.txt", "a") as log_file:
+            log_file.write(
+                f"{time.strftime('%Y-%m-%d %H:%M:%S')} - Critical Temperature: {self.temp}\n"
+            )
+        self.count += 1
+
+    def classifier(self):
+
+        if self.temp <= self.NORMAL:
+            print(f"{time.ctime()} : {self.temp} C -- Normal")
+        elif self.temp <= self.MODERATE:
+            print(f"{time.ctime()} : {self.temp} C -- Moderate")
+        else:
+            print(f"{time.ctime()} : {self.temp} C -- Critical")
+            self.log_writer()
+
+    def monitor(self):
+
+        self.get_temperature()
+
+        self.classifier()
+
+        time.sleep(2)
+
 
 system1 = TemperatureSensor(1)
 print("System ID : ", system1.sensor_id)
-for i in range(5, 11) : 
-  system1.monitor()
+for i in range(5, 11):
+    system1.monitor()
 
 print(f"No. of critical incidents from {start} to {time.ctime()} : {system1.count}")
